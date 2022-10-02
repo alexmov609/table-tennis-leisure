946b56730fcb718dc57d6e31ecb553553a00ab03
“Yaroslav <kutsiaroslav@gmail.com>
2022-10-02 14:49:25 +0300

    Adds 'theme' renewal after page refresh

--- client/src/custom_hooks/useRefreshToken.js
+++ client/src/custom_hooks/useRefreshToken.js
@@ -1,13 +1,13 @@
 import { useStateContext } from "../contexts/ContextProvider";
 
 const useRefreshToken = () => {
-  const { setAuthentication } = useStateContext();
+  const { setAuthentication, setCurrentMode } = useStateContext();
   const refresh = async () => {
     const refreshData = await (
       await fetch(process.env.REACT_APP_REFRESH_TOKEN)
     ).json();
     if (!refreshData) throw new Error("refreshData is empty");
-    const { authorities, accessToken } = refreshData;
+    const { authorities, accessToken, theme } = refreshData;
     setAuthentication((prev) => {
       return {
         ...prev,
@@ -15,6 +15,7 @@ const useRefreshToken = () => {
         accessToken,
       };
     });
+    setCurrentMode(theme === 1 ? "light" : "dark");
     return accessToken;
   };
   return refresh;
